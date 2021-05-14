efc1e889c826cb2dbff7c4cc5d2a685c88488c9a
BarryWin <slavon.v21@gmail.com>
2021-05-14 16:31:18 +0300

    tables language

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -1,31 +1,45 @@
-(function($) {
+(function ($) {
 
-	"use strict";
+    "use strict";
 
-	var fullHeight = function() {
+    var fullHeight = function () {
 
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function () {
+            $('.js-fullheight').css('height', $(window).height());
+        });
 
-	};
-	fullHeight();
+    };
+    fullHeight();
 
-	$('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
-  });
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
 
 })(jQuery);
 
-$(document).ready( function () {
-    $('#table_id').DataTable();
-} );
+$(document).ready(function () {
+    $('#table_id').DataTable({
+        "language": {
+            "lengthMenu": "Показывает _MENU_ записей на странице",
+            "zeroRecords": "Ничего не найдено - сори",
+            "emptyTable": "В таблицы нет данных",
+            "info": "Показано _END_ из _TOTAL_ записей",
+            "infoEmpty": "Нет доступных записей",
+            "infoFiltered": "(Выбрано из _MAX_ записей)",
+            "search": "Поиск:",
+            "paginate": {
+                "previous": "Предыдущая страница",
+                "next": "Следующая страница",
+            }
+        }
+    });
+});
 
-$(document).ready( function () {
+$(document).ready(function () {
     $('#documents').DataTable();
-} );
+});
 
-$(document).ready( function () {
+$(document).ready(function () {
     $('#next-lessons').DataTable();
-} );
+});
