16167881a0931effd7eefe9386c9eca766458cdc
BarryWin <slavon.v21@gmail.com>
2021-05-24 04:43:33 +0300

    schedule lesson update

--- driving/settings.py
+++ driving/settings.py
@@ -35,8 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'schedule',
-    'djangobower',
 ]
 
 MIDDLEWARE = [
@@ -118,22 +116,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "main/static",
-]
-
-STATICFILES_FINDERS = [
-    'djangobower.finders.BowerFinder',
+    BASE_DIR / "main/../main/../main/static",
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-BOWER_COMPONENTS_ROOT = '/driving/components/'
-
-BOWER_INSTALLED_APPS = (
-    'jquery',
-    'jquery-ui',
-    'bootstrap'
-)
